6d46d865b18ca22c4f96272fe265543d3feb3f09
Tim Staley <git@timstaley.co.uk>
2016-03-19 19:37:22 +0000

    Change AMI declination limit to 0.0.

--- fourpisky/filters.py
+++ fourpisky/filters.py
@@ -4,7 +4,7 @@ from fourpisky.visibility import DEG_PER_RADIAN
 class ami:
     @staticmethod
     def reject(position):
-        dec_limit = 15.0
+        dec_limit = 0.0
         if position.dec*DEG_PER_RADIAN < dec_limit:
             return "Target below declination limit of %s degrees" % dec_limit
         return None
