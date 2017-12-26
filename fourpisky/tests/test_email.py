d80b9210cc94e47dab0edb1b064f355c8293f6c1
Tim Staley <git@timstaley.co.uk>
2017-12-26 23:50:53 +0000

    Fix test-skip

--- fourpisky/tests/test_email.py
+++ fourpisky/tests/test_email.py
@@ -2,9 +2,10 @@ from fourpisky.comms.email import send_email
 from fourpisky.local import contacts
 import pytest
 
-pytest.mark.skipif(contacts.sendgrid_api_key is None,
+@pytest.mark.skipif(contacts.sendgrid_api_key is None,
                    reason="No SendGrid API Key supplied")
 def test_email_send():
+    print "KEY", contacts.sendgrid_api_key
     response = send_email(
         recipient_addresses=[c.email for c in contacts.error_contacts],
         subject="[TEST] Unit-testing email for fourpisky-core (again)",
